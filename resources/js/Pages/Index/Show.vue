550e0e76acff3941407b5ddd858931082d71f573
herkanuzun <herkanuzun@gmail.com>
2023-10-15 12:11:27 +0300

    rebuild fix issues to docker db

---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>Show</div>
+    <Link :href="route('index')">Create User</Link>
+  </template>
+  
+  <script setup>
+  import { Link } from '@inertiajs/vue3'
+  </script>
---
+++ resources/js/app.js
@@ -0,0 +1,35 @@
+import '../css/app.css';
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { InertiaProgress } from '@inertiajs/progress'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+InertiaProgress.init({
+    delay: 0,
+    color: '#29d',
+    includeCSS: true,
+    showSpinner: true,
+  })
+  
+  createInertiaApp({
+    resolve: async (name) => {
+      const pages = import.meta.glob('./Pages/**/*.vue')
+  
+      const page = await pages[`./Pages/${name}.vue`]()
+      page.default.layout = page.default.layout 
+  
+      return page
+    },
+    // function () {}
+    // {el: xxx, App: xxx, props: xxx}
+    setup({ el, App, props, plugin }) {
+      createApp({ render: () => h(App, props) })
+        .use(plugin)
+        .use(ZiggyVue)
+        .mount(el)
+    },
+  })
