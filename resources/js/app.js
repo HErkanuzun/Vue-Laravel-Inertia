3d4d57f22fff616c0c70924063979d175fe04836
herkanuzun <herkanuzun@gmail.com>
2023-10-17 23:06:15 +0300

    eslint import sc

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { InertiaProgress } from '@inertiajs/progress'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import MainLayout from './Layouts/MainLayout.vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
@@ -20,7 +21,7 @@ InertiaProgress.init({
       const pages = import.meta.glob('./Pages/**/*.vue')
   
       const page = await pages[`./Pages/${name}.vue`]()
-      page.default.layout = page.default.layout 
+      page.default.layout = page.default.layout || MainLayout
   
       return page
     },
